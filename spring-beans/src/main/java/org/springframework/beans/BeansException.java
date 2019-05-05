b28f4a0d1a2ddad22323b2d3a0ffac2e07e0a989
admin <1178919633@qq.com>
2019-05-05 22:06:19 +0800

    Initial commit

---
+++ spring-beans/src/main/java/org/springframework/beans/BeansException.java
@@ -0,0 +1,27 @@
+package org.springframework.beans;
+
+import org.springframework.core.NestedRuntimeException;
+import org.springframework.lang.Nullable;
+
+
+/**
+ * Abstract superclass for all exceptions thrown in the beans package
+ * and subpackages.
+ *
+ * <p>Note that this is a runtime (unchecked) exception. Beans exceptions
+ * are usually fatal; there is no reason for them to be checked.
+ *
+ * @author Rod Johnson
+ * @author Juergen Hoeller
+ */
+public class BeansException extends NestedRuntimeException {
+
+
+    public BeansException(String msg) {
+        super(msg);
+    }
+
+    public BeansException(@Nullable String msg, @Nullable Throwable cause) {
+        super(msg, cause);
+    }
+}
