842636ec5f9238745de5afe953990dd35f45a9c2
1178919633@qq.com <117819633@qq.com>
2019-05-08 18:24:42 +0800

    添加beanfactory

---
+++ spring-beans/src/main/java/org/springframework/beans/factory/support/DefaultSingletonBeanRegistry.java
@@ -0,0 +1,92 @@
+package org.springframework.beans.factory.support;
+
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.config.SingletonBeanRegistry;
+import org.springframework.core.SimpleAliasRegistry;
+import org.springframework.lang.Nullable;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Generic registry for shared bean instances, implementing the
+ * {@link org.springframework.beans.factory.config.SingletonBeanRegistry}.
+ * Allows for registering singleton instances that should be shared
+ * for all callers of the registry, to be obtained via bean name.
+ *
+ * <p>Also supports registration of
+ * {@link org.springframework.beans.factory.DisposableBean} instances,
+ * (which might or might not correspond to registered singletons),
+ * to be destroyed on shutdown of the registry. Dependencies between
+ * beans can be registered to enforce an appropriate shutdown order.
+ *
+ * <p>This class mainly serves as base class for
+ * {@link org.springframework.beans.factory.BeanFactory} implementations,
+ * factoring out the common management of singleton bean instances. Note that
+ * the {@link org.springframework.beans.factory.config.ConfigurableBeanFactory}
+ * interface extends the {@link SingletonBeanRegistry} interface.
+ *
+ * <p>Note that this class assumes neither a bean definition concept
+ * nor a specific creation process for bean instances, in contrast to
+ * {@link AbstractBeanFactory} and {@link DefaultListableBeanFactory}
+ * (which inherit from it). Can alternatively also be used as a nested
+ * helper to delegate to.
+ *
+ * @author Juergen Hoeller
+ * @since 2.0
+ * @see #registerSingleton
+ * @see #registerDisposableBean
+ * @see org.springframework.beans.factory.DisposableBean
+ * @see org.springframework.beans.factory.config.ConfigurableBeanFactory
+ */
+public class DefaultSingletonBeanRegistry extends SimpleAliasRegistry implements SingletonBeanRegistry {
+
+    private final Map<String,Object> singletonObjects = new ConcurrentHashMap<>(256);
+
+    private final Map<String, ObjectFactory<?>> singletonFactories = new HashMap<>(16);
+
+    private final Map<String,Object> earlySingletonObjects = new HashMap<>(16);
+
+    private final Set<String> registeredSingletons = new LinkedHashSet<>(256);
+
+    private final Set<String> singletonsCurrenttlyIncreation =
+            Collections.newSetFromMap(new ConcurrentHashMap<>(16));
+
+    private final Set<String> inCreationCheckExclusions =
+            Collections.newSetFromMap(new ConcurrentHashMap<>(16));
+
+    @Nullable
+    private Set<Exception> suppressedExceptions;
+
+
+
+    @Override
+    public void registerSingleton(String beanName, Object singletonObject) {
+
+    }
+
+    @Override
+    public Object getSingleton(String beanName) {
+        return null;
+    }
+
+    @Override
+    public boolean containsSingleton(String beanName) {
+        return false;
+    }
+
+    @Override
+    public String[] getSingletonNames() {
+        return new String[0];
+    }
+
+    @Override
+    public int getSingletonCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getSingletonMutex() {
+        return null;
+    }
+}
